dc5939dbb98b36ef2fa02ebd24370c2560336dd0
Alea Cobham <Alea Cobham>
2017-01-30 19:12:17 -0500

    Initial commit

---
+++ app/src/main/java/alea/aleademo/Adapter/ListViewAdapter.java
@@ -0,0 +1,79 @@
+package alea.aleademo.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import alea.aleademo.R;
+
+/**
+ * Created by aleac on 1/25/2017.
+ */
+
+public class ListViewAdapter extends BaseAdapter {
+    private Context mContext;
+    private final LayoutInflater mInflater;
+    @Override
+    public int getCount() {
+        return 100;
+    }
+    public ListViewAdapter(Context context) {
+        mContext = context;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+//        TextView view=new TextView(mContext);
+//        view.setText(String.valueOf(position))
+//        return view;
+//    }
+        ViewHolder holder;
+        if (convertView==null){
+            convertView = mInflater.inflate(R.layout.list_item, parent, false);
+            holder = new ViewHolder();
+            holder.textView1 = (TextView)convertView.findViewById(R.id.list_view_tv1);
+            holder.textView2 = (TextView)convertView.findViewById(R.id.list_view_tv2);
+            holder.textView3 = (TextView)convertView.findViewById(R.id.list_view_tv3);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag(); //got an error @ first because get tag returns an object
+        }
+        holder.textView1.setText(String.valueOf(position));
+        holder.textView3.setText(String.valueOf(position));
+
+        if(position%2==0){
+            holder.textView1.setVisibility(View.VISIBLE);
+            holder.textView3.setVisibility(View.INVISIBLE);
+        }else{
+            holder.textView1.setVisibility(View.INVISIBLE);
+            holder.textView3.setVisibility(View.VISIBLE);
+        }
+
+//        View rowView = mInflater.inflate(R.layout.list_item, parent, false);
+//        TextView textView = (TextView)rowView.findViewById(R.id.list_view_tv);
+//        textView.setText(String.valueOf(position));
+//        return rowView;
+        return convertView;
+    }
+}
+class ViewHolder{
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+}
---
+++ app/src/main/java/alea/aleademo/ListViewActivity.java
@@ -0,0 +1,35 @@
+package alea.aleademo;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import alea.aleademo.Adapter.ListViewAdapter;
+import alea.aleademo.R;
+
+public class ListViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+        initialView();
+    }
+
+    private void initialView() {
+        listView = (ListView) findViewById(R.id.list_view);
+        ListViewAdapter listViewAdapter = new ListViewAdapter(this);
+        listView.setAdapter(listViewAdapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, "listView was clicked at position: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
